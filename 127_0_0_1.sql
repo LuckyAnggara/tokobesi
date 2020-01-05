cf3f1fe8a24c0b31e253a7fad43c4b5410110dfd
joyLASTnight <joyLASTnight>
2020-01-05 22:13:39 +0700

    database

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2020 at 10:50 AM
--- Server version: 10.3.16-MariaDB
--- PHP Version: 7.3.6
+-- Generation Time: Jan 05, 2020 at 04:13 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,10 +32,18 @@ USE `tob`;
 
 CREATE TABLE `master_barang` (
   `kode_barang` varchar(255) NOT NULL,
+  `tipe_barang` int(11) DEFAULT 0,
+  `jenis_barang` int(11) DEFAULT 0,
+  `merek_barang` int(11) DEFAULT 0,
+  `kode_supplier` varchar(128) DEFAULT NULL,
   `nama_barang` varchar(255) NOT NULL,
+  `harga_pokok` double NOT NULL,
   `harga_satuan` double NOT NULL,
-  `satuan` varchar(20) NOT NULL,
+  `kode_satuan` int(11) DEFAULT 0,
+  `persediaan_minimum` int(11) NOT NULL DEFAULT 0,
   `gambar` varchar(255) NOT NULL,
+  `keterangan` text NOT NULL,
+  `status_jual` tinyint(4) NOT NULL,
   `tanggal_input` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -43,15 +51,54 @@ CREATE TABLE `master_barang` (
 -- Dumping data for table `master_barang`
 --
 
-INSERT INTO `master_barang` (`kode_barang`, `nama_barang`, `harga_satuan`, `satuan`, `gambar`, `tanggal_input`) VALUES
-('B002', 'BATU KAPUR', 5000, 'POTONG', '0', '2019-12-31 11:13:16'),
-('B003', 'BATU AKIK', 10000, 'pieces', 'default.jpg', '2019-12-31 00:00:00'),
-('B004', 'Batu Awan', 1000, 'pieces', 'default.jpg', '2019-12-31 00:00:00'),
-('B005', 'BATU BATUAN', 10000, 'pieces', 'default.jpg', '2019-12-31 00:00:00'),
-('B006', 'BATU BATU BATU', 1000, 'pieces', 'default.jpg', '2019-12-31 00:00:00'),
-('B007', 'BATU LAUT', 100000, 'kilogram', 'default.jpg', '2019-12-31 00:00:00'),
-('B008', 'BB', 11, 'meter', 'B008.jpg', '2019-12-31 00:00:00'),
-('B009', 'BATU TUMPENG', 123123123, 'pieces', 'B009.png', '2019-12-31 00:00:00');
+INSERT INTO `master_barang` (`kode_barang`, `tipe_barang`, `jenis_barang`, `merek_barang`, `kode_supplier`, `nama_barang`, `harga_pokok`, `harga_satuan`, `kode_satuan`, `persediaan_minimum`, `gambar`, `keterangan`, `status_jual`, `tanggal_input`) VALUES
+('B001', 1, 0, 0, 'EKO076', 'BESI BETON', 100000, 150000, 0, 10, 'B001.jpg', '', 1, '2020-01-05 16:10:04'),
+('S001', 1, NULL, 1, NULL, 'SPANDEK', 0, 100000, 1, 0, 'default.jpg', 'asdasdasd', 1, '2020-01-05 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_jenis_barang`
+--
+
+CREATE TABLE `master_jenis_barang` (
+  `id_jenis_barang` int(11) NOT NULL,
+  `kode_jenis_barang` varchar(128) NOT NULL,
+  `nama_jenis_barang` varchar(255) NOT NULL,
+  `keterangan` text NOT NULL,
+  `tanggal_input` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `master_jenis_barang`
+--
+
+INSERT INTO `master_jenis_barang` (`id_jenis_barang`, `kode_jenis_barang`, `nama_jenis_barang`, `keterangan`, `tanggal_input`) VALUES
+(0, 'NULL', 'TIDAK', 'TIDAK ADA JENIS BARANG', '2020-01-05 08:11:36'),
+(1, 'BSI', 'BESI', 'Jenis Barang ', '2020-01-05 05:39:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_merek_barang`
+--
+
+CREATE TABLE `master_merek_barang` (
+  `id_merek_barang` int(11) NOT NULL,
+  `kode_merek_barang` varchar(128) NOT NULL,
+  `nama_merek_barang` varchar(255) NOT NULL,
+  `keterangan` text NOT NULL,
+  `tanggal_input` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `master_merek_barang`
+--
+
+INSERT INTO `master_merek_barang` (`id_merek_barang`, `kode_merek_barang`, `nama_merek_barang`, `keterangan`, `tanggal_input`) VALUES
+(0, 'NULL', 'TIDAK ADA MEREK', 'TIDAK ADA MEREK BARANG', '2020-01-05 00:00:00'),
+(1, 'ATM', 'ANTAM', 'MEREK BESI', '2020-01-05 00:00:00'),
+(6, 'KTK', 'KERAKATAU STEEL', 'BESI KRAKATAU STEEL', '2020-01-05 08:05:10');
 
 -- --------------------------------------------------------
 
@@ -64,17 +111,89 @@ CREATE TABLE `master_persediaan` (
   `kode_barang` varchar(128) NOT NULL,
   `jumlah_persediaan` int(15) NOT NULL,
   `jumlah_keranjang` double NOT NULL,
-  `jumlah_persediaan_sementara` int(15) NOT NULL COMMENT 'temporary jumlah persediaan setelah di pesan'
+  `jumlah_persediaan_sementara` int(15) NOT NULL COMMENT 'temporary jumlah persediaan setelah di pesan',
+  `tanggal_input` datetime NOT NULL,
+  `no_order_terakhir` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `master_persediaan`
 --
 
-INSERT INTO `master_persediaan` (`id`, `kode_barang`, `jumlah_persediaan`, `jumlah_keranjang`, `jumlah_persediaan_sementara`) VALUES
-(3, 'B002', 100, 0, 0),
-(4, 'B004', 0, 0, 0),
-(5, 'B003', 100, 0, 0);
+INSERT INTO `master_persediaan` (`id`, `kode_barang`, `jumlah_persediaan`, `jumlah_keranjang`, `jumlah_persediaan_sementara`, `tanggal_input`, `no_order_terakhir`) VALUES
+(9, 'B001', 0, 0, 0, '2020-01-05 09:39:46', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_satuan_barang`
+--
+
+CREATE TABLE `master_satuan_barang` (
+  `id_satuan` int(11) NOT NULL,
+  `kode_satuan` varchar(255) NOT NULL,
+  `nama_satuan` varchar(255) NOT NULL,
+  `keterangan` text NOT NULL,
+  `tanggal_input` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `master_satuan_barang`
+--
+
+INSERT INTO `master_satuan_barang` (`id_satuan`, `kode_satuan`, `nama_satuan`, `keterangan`, `tanggal_input`) VALUES
+(0, 'PCS', 'PIECES', 'Untuk produk satuan seperti Gulaasdasda', '2020-01-05 08:20:51'),
+(1, 'PTG', 'POTONG', '', '0000-00-00 00:00:00'),
+(2, 'MTR', 'METER', '', '0000-00-00 00:00:00'),
+(3, 'CM', 'CENTIMETER', 'Untuk alsdklaskdasd', '2020-01-04 08:02:14'),
+(4, 'PCS', 'PIECES', 'Untuk produk satuan seperti Gula', '2020-01-05 05:24:09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_supplier`
+--
+
+CREATE TABLE `master_supplier` (
+  `kode_supplier` varchar(128) NOT NULL,
+  `nama_supplier` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `nomor_telepon` varchar(128) NOT NULL,
+  `npwp` varchar(255) NOT NULL,
+  `nomor_rekening` text NOT NULL,
+  `keterangan` text NOT NULL,
+  `tanggal_input` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `master_supplier`
+--
+
+INSERT INTO `master_supplier` (`kode_supplier`, `nama_supplier`, `alamat`, `nomor_telepon`, `npwp`, `nomor_rekening`, `keterangan`, `tanggal_input`) VALUES
+('EKO076', 'PT JAYA MA', 'aksdjlkasjdlkasjdlkaj\r\nasdasdasd\r\nasdasdasdasd', '921839018239081', '12.983.910.2-839.012', 'BCA-192839128-Lucky', 'asdasdasd', '2020-01-04 05:03:53'),
+('QHQ851', 'PTP', 'TPPTPTTPPTPTPTPT', '123012301203', '11.111.111.1-111.111', 'BCA-12310230122-1231231231', 'paspdapdasdasdasda', '2020-01-05 06:26:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master_tipe_barang`
+--
+
+CREATE TABLE `master_tipe_barang` (
+  `id_tipe` int(11) NOT NULL,
+  `nama_tipe` varchar(255) NOT NULL,
+  `keterangan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `master_tipe_barang`
+--
+
+INSERT INTO `master_tipe_barang` (`id_tipe`, `nama_tipe`, `keterangan`) VALUES
+(1, 'INVENTORY', 'TIPE UNTUK BARANG YANG DIJUAL'),
+(2, 'NON-INVENTORY', 'TIPE UNTUK BARANG YANG TIDAK DIJUAL, CONTOH PERALATAN KANTOR'),
+(3, 'JASA', 'TIPE BARANG YANG TIDAK BERUPA, SEPERTI JASA PENGANTAR BARANG, JASA RAKITAN'),
+(4, 'RAKITAN', 'TIPE BARANG BUNDLING, SEPERTI PAKET SEMABAKO');
 
 -- --------------------------------------------------------
 
@@ -122,6 +241,13 @@ CREATE TABLE `tabel_keranjang_temp` (
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tabel_keranjang_temp`
+--
+
+INSERT INTO `tabel_keranjang_temp` (`id`, `no_order`, `no_keranjang`, `id_pelanggan`, `kode_barang`, `jumlah_pembelian`, `harga_total`, `timestamp`) VALUES
+(57, '0916235847', 0, '4545', 'B002 ', 50, 250000, '2020-01-03 00:38:00');
+
 -- --------------------------------------------------------
 
 --
@@ -142,17 +268,6 @@ CREATE TABLE `tabel_pelanggan` (
 INSERT INTO `tabel_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `alamat`, `nomor_telepon`) VALUES
 ('4545', 'Lucky Anggara', 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', '082116562811');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tabel_satuan`
---
-
-CREATE TABLE `tabel_satuan` (
-  `id` int(11) NOT NULL,
-  `satuan` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 --
 -- Indexes for dumped tables
 --
@@ -161,7 +276,25 @@ CREATE TABLE `tabel_satuan` (
 -- Indexes for table `master_barang`
 --
 ALTER TABLE `master_barang`
-  ADD PRIMARY KEY (`kode_barang`);
+  ADD PRIMARY KEY (`kode_barang`),
+  ADD KEY `tipe_barang` (`tipe_barang`,`jenis_barang`,`merek_barang`),
+  ADD KEY `satuan` (`kode_satuan`),
+  ADD KEY `merek_barang_join` (`merek_barang`),
+  ADD KEY `jenis_barang_join` (`jenis_barang`),
+  ADD KEY `kode_supplier` (`kode_supplier`);
+
+--
+-- Indexes for table `master_jenis_barang`
+--
+ALTER TABLE `master_jenis_barang`
+  ADD PRIMARY KEY (`id_jenis_barang`),
+  ADD UNIQUE KEY `kode_jenis` (`kode_jenis_barang`);
+
+--
+-- Indexes for table `master_merek_barang`
+--
+ALTER TABLE `master_merek_barang`
+  ADD PRIMARY KEY (`id_merek_barang`);
 
 --
 -- Indexes for table `master_persediaan`
@@ -169,6 +302,24 @@ ALTER TABLE `master_barang`
 ALTER TABLE `master_persediaan`
   ADD PRIMARY KEY (`id`),
   ADD KEY `kode_barang` (`kode_barang`);
+
+--
+-- Indexes for table `master_satuan_barang`
+--
+ALTER TABLE `master_satuan_barang`
+  ADD PRIMARY KEY (`id_satuan`);
+
+--
+-- Indexes for table `master_supplier`
+--
+ALTER TABLE `master_supplier`
+  ADD PRIMARY KEY (`kode_supplier`);
+
+--
+-- Indexes for table `master_tipe_barang`
+--
+ALTER TABLE `master_tipe_barang`
+  ADD PRIMARY KEY (`id_tipe`);
 
 --
 -- Indexes for table `tabel_daftar_belanja`
@@ -195,20 +346,38 @@ ALTER TABLE `tabel_pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`);
 
 --
--- Indexes for table `tabel_satuan`
---
-ALTER TABLE `tabel_satuan`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `master_jenis_barang`
+--
+ALTER TABLE `master_jenis_barang`
+  MODIFY `id_jenis_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `master_merek_barang`
+--
+ALTER TABLE `master_merek_barang`
+  MODIFY `id_merek_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `master_persediaan`
 --
 ALTER TABLE `master_persediaan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `master_satuan_barang`
+--
+ALTER TABLE `master_satuan_barang`
+  MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `master_tipe_barang`
+--
+ALTER TABLE `master_tipe_barang`
+  MODIFY `id_tipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tabel_keranjang`
@@ -220,17 +389,21 @@ ALTER TABLE `tabel_keranjang`
 -- AUTO_INCREMENT for table `tabel_keranjang_temp`
 --
 ALTER TABLE `tabel_keranjang_temp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
-
---
--- AUTO_INCREMENT for table `tabel_satuan`
---
-ALTER TABLE `tabel_satuan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `master_barang`
+--
+ALTER TABLE `master_barang`
+  ADD CONSTRAINT `jenis_barang_join` FOREIGN KEY (`jenis_barang`) REFERENCES `master_jenis_barang` (`id_jenis_barang`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `kode_supplier_join` FOREIGN KEY (`kode_supplier`) REFERENCES `master_supplier` (`kode_supplier`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `merek_barang_join` FOREIGN KEY (`merek_barang`) REFERENCES `master_merek_barang` (`id_merek_barang`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `satuan_barang_join` FOREIGN KEY (`kode_satuan`) REFERENCES `master_satuan_barang` (`id_satuan`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `tipe_barang_join` FOREIGN KEY (`tipe_barang`) REFERENCES `master_tipe_barang` (`id_tipe`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `master_persediaan`
